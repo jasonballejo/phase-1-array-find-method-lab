e83d1400778c58128fbf489a1c58c105f8110c51
Jason Ballejo <ballejo19@gmail.com>
2022-07-30 17:43:39 -0400

    commit tree cf2a9e63c0e8a644b94fe2e897871f60b178f8f3

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+function superbowlWin(record){
+    const name = record.find(function (recordObj){
+        return recordObj.result === "W"
+    })
+    return name === undefined ? name : name.year
+}
